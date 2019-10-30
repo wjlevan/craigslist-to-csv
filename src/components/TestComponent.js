42f151cbb7bc469076ae82a29a39bf14e287408e
wlevan <wjlevan@gmail.com>
2019-10-30 14:16:27 -0700

    working csv download

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { makeRequest, parseData, visitLinks } from './functions';
-import { ExportCSV } from './ExcelComponent';
+import './TestComponent.css';
+import FileSaver from 'file-saver';
 
 export class TestComponent extends Component {
     constructor(props) {
@@ -18,20 +19,33 @@ componentDidMount() {
     .then(response => parseData(response))
     .then(links => visitLinks(links))
     .then(dataset => setTimeout(() => {
-        dataset = dataset.map(row => row.join('; '))
+        // dataset = dataset.map(row => row.join('; '))
         // console.log(dataset[0])
         this.setState({
             dataset: dataset})
-    }, 5000))
 
+        let newstring = dataset.map((row, i) => row + "\n")
+        var file = new File(newstring, "test.csv", {type: "text/plain;charset=utf-8"});
+        FileSaver.saveAs(file);     
+        console.log(dataset)
+    }, 5000))
+    
 }
 
     render() {
         return(
-            <div>
-                    < ExportCSV />
-                {this.state.dataset.map((el, index) => <div key={index}>{el}<br/><br/></div> )}
-
+            <div className="display">
+                    {/* < ExportCSV /> */}
+                    <table>
+                        <tbody>
+                            {this.state.dataset.map((row, i) => 
+                                <tr key={i}>
+                                    {/* {console.log(row)} */}
+                                    {row.map((col, j) => <td key={j}>{col}</td>)}
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
             </div>
         )
     }
